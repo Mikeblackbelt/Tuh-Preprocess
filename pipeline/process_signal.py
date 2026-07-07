26d59cc9f5389ffa3168436cbe590c436254039e
Crystal-Lin1 <crunchytvtable@gmail.com>
2026-07-07 19:49:18 -0400

    feat: standardize channels

--- pipeline/process_signal.py
+++ pipeline/process_signal.py
@@ -1,15 +1,20 @@
 import mne
 import pandas as pd
+from util import handle_logs
+from testing.helpers import *
 
-def load_edf_metadata(path):
-    raw = mne.io.read_raw_edf(path, preload=False)
+logger = handle_logs.get_logger("process_signal", "logs/app.log")
+
+def load_edf(path):
+    raw = mne.io.read_raw_edf(path, preload=False, verbose=False)
     metadata = pd.DataFrame({
+        "path": path,
         "channels": raw.ch_names,
         "sfreq": raw.info["sfreq"],
         "n_samples": raw.n_times,
         "duration_sec": raw.n_times / raw.info["sfreq"]
     })
-    return metadata
+    return raw, metadata
 
 def split_into_epochs(edf_path, epoch_duration=30):
     """
@@ -43,3 +48,32 @@ def split_into_epochs(edf_path, epoch_duration=30):
     )
     return epochs
 
+def standardize_channels(raw, metadata):
+
+    channel_map = {}
+    for ch in raw.ch_names:
+        new_name = ch.replace('EEG ', '')
+        # Remove reference suffixes: -LE (linked ears), -REF (average reference), 
+        new_name = new_name.replace('-LE', '')
+        new_name = new_name.replace('-REF', '')
+        channel_map[ch] = new_name
+    
+    raw.rename_channels(channel_map)
+
+    # These are the 19 standard channels that cover every major brain region
+    desired_order = [
+        'FP1', 'FP2', 'F7', 'F3', 'FZ', 'F4', 'F8',
+        'T3', 'C3', 'CZ', 'C4', 'T4',
+        'T5', 'P3', 'PZ', 'P4', 'T6',
+        'O1', 'O2'
+    ]
+    
+    missing = [ch for ch in desired_order if ch not in raw.ch_names]
+    
+    if missing:
+        filename = metadata['path'].iloc[0] if isinstance(metadata['path'], pd.Series) else metadata['path']
+        logger.warning(f'{filename} missing {len(missing)} channels: {missing}')
+        return None 
+    
+    raw.reorder_channels(desired_order)
+    return raw
